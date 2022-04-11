f8ef839a91dd1324bcd0266cf15befdcdaa9c673
guleixibian <guleixibian@outlook.com>
2022-04-11 14:36:22 +0800

    Add "tag-badges-after-heading"

--- assets/JavaScript/CodeLoader.js
+++ assets/JavaScript/CodeLoader.js
@@ -15,7 +15,7 @@ jQuery(function(){
                 var codeExample = parser.parseFromString(response,"text/xml");
                 var code = codeExample.getElementsByTagName("codeExample")[0].innerHTML;
                 $(element).find("code").text(code);
-                Example.innerHTML = code;
+                Example.innerHTML = code.replace(/Lorem ipsum.../,"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
                 setTimeout(Prism.highlightAll(), 100)
             };
             codeReq.open("GET",`codeExamples/${codeLink}.xml`);
