0c32036b6b89849cd5d27218865f1c02487e620b
guleixibian <guleixibian@outlook.com>
2022-04-11 08:36:18 +0800

    Stop using <iframe>s for network speed

--- assets/JavaScript/CodeLoader.js
+++ assets/JavaScript/CodeLoader.js
@@ -1,43 +1,25 @@
 //Code Loader
 "use strict";
-$(document).ready(function(){
+
+jQuery(function(){
     const CodeBlocks = document.getElementsByClassName("code-load");
     if (CodeBlocks != null){
         for (let i = 0; i < CodeBlocks.length; i++) {
             const element = CodeBlocks[i];
             var codeLink = $(element).attr("data-code-link");
+            const Example = document.getElementsByName(codeLink)[0]
             var codeReq = new XMLHttpRequest();
             codeReq.onloadend = function(){
-                var responseText = this.responseText;
+                var response = this.response;
                 var parser = new DOMParser();
-                var codeExample = parser.parseFromString(responseText,"text/xml");
+                var codeExample = parser.parseFromString(response,"text/xml");
                 var code = codeExample.getElementsByTagName("codeExample")[0].innerHTML;
-                $(element).find("code").html(code);
+                $(element).find("code").text(code);
+                Example.innerHTML = code;
                 setTimeout(Prism.highlightAll(), 100)
             };
             codeReq.open("GET",`codeExamples/${codeLink}.xml`);
             codeReq.send();
         }
     }
-});
-
-//iframe height helper
-$(document).ready(function(){
-    function SetHeight(){
-        const iframe = document.getElementsByTagName("iframe");
-        if (iframe != null){
-            for (let i = 0; i < iframe.length; i++) {
-                const el = iframe[i];
-                $(el).ready(function(){
-                    var height = el.contentDocument.documentElement.offsetHeight;
-                    el.height = `${height}`;
-                }); 
-            }
-        }
-    }
-    SetHeight();
-
-    $(window).resize(function(){
-        SetHeight();
-    });
 });
