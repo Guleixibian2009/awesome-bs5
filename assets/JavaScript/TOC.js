f898bbe7394ff04aa5053edc16a67bbc37823500
guleixibian <guleixibian@outlook.com>
2022-04-12 16:15:43 +0800

    Add numbers for TOC

--- assets/JavaScript/TOC.js
+++ assets/JavaScript/TOC.js
@@ -13,19 +13,26 @@ $(document).ready(function(){
     }
     //Next turn them into <li> and give them correct classes
     var OutputList = `<div class="sticky-top pt-2"><div class="heading d-grid" style="place-content: center !important;"><p class="h3 lead">Table Of Contents</p></div><ul id="TOC" class="list-unstyled">`
+    var heading=0, subheading=0, subsubheading=0;
     HeadingList.forEach(el => {
         if (el.nodeName == "H2"){
+            heading+=1
+            var outputNum = `${heading}. `
             var outputClass = "ps-2";
             el.id = $(el).text();
         }
         else if (el.nodeName == "H3"){
+            subheading+=1
+            var outputNum = `${heading}.${subheading} `
             var outputClass = "ps-3";
             el.id = $(el).text();
         } else if (el.nodeName == "H4"){
+            subsubheading+=1
+            var outputNum = `${heading}.${subheading}.${subsubheading} `
             var outputClass = "ps-4";
             el.id = $(el).text();
         }
-        var output = `<li class="${outputClass}"><button class="text-secondary btn btn-sm btn-outline-light p-1 border-0 rounded">${el.innerHTML}</button></li>`
+        var output = `<li class="${outputClass}"><button class="text-secondary btn btn-sm btn-outline-light p-1 border-0 rounded">${outputNum}${el.innerHTML}</button></li>`
         OutputList = OutputList + output;
     });
     OutputList = OutputList + `</ul></div>`;
@@ -55,7 +62,8 @@ $(document).ready(function(){
 
     //When clicked, get the id and it's YOffSet
     $("nav#TOC li").click(function(){
-        var raw = $(this).text();
+        var raw = $(this).text().replace(/\d.\d.\d\s/,"").replace(/\d.\d\s/,"").replace(/\d.\s/,"");
+        console.log(raw);
         var heading = document.getElementById(raw);
         var headingY = heading.offsetTop;
         //Use jQuery.scrollTo to create a linear animation
