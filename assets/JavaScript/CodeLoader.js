16acae696ec5c5eb2e2deea04dcb153f73d374e0
guleixibian <guleixibian@outlook.com>
2022-04-14 16:28:56 +0800

    Change XML to XHTML for building convenience and avoid CE

--- assets/JavaScript/CodeLoader.js
+++ assets/JavaScript/CodeLoader.js
@@ -2,23 +2,32 @@
 "use strict";
 
 jQuery(function(){
+    $.ajaxSetup({
+        contentType: "text/html",
+        scriptCharset: "UTF-8"
+    })
     const CodeBlocks = document.getElementsByClassName("code-load");
     if (CodeBlocks != null){
         for (let i = 0; i < CodeBlocks.length; i++) {
             const element = CodeBlocks[i];
             var codeLink = $(element).attr("data-code-link");
-            const Example = document.getElementsByName(codeLink)[0]
+            const Example = document.getElementsByName(codeLink)[0];
             var codeReq = new XMLHttpRequest();
             codeReq.onloadend = function(){
-                var response = this.response;
-                var parser = new DOMParser();
-                var codeExample = parser.parseFromString(response,"text/html");
-                var code = codeExample.getElementsByTagName("main")[0].innerHTML;
-                $(element).find("code").text(code);
-                Example.innerHTML = code.replace(/Lorem ipsum.../,"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
-                setTimeout(Prism.highlightAll(), 100)
+                try {
+                    var response = this.response;
+                    var parser = new DOMParser();
+                    var codeExample = parser.parseFromString(response,"text/html");
+                    var code = codeExample.getElementsByTagName("main")[0].innerHTML;
+                    $(element).find("code").text(code);
+                    Example.innerHTML = code.replace(/Lorem ipsum.../,"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
+                    setTimeout(Prism.highlightAll(), 100);
+                } catch (error) {
+                    //I don't want those error messages!
+                }
+                
             };
-            codeReq.open("GET",`iframes/${codeLink}.html`);
+            codeReq.open("GET",`examples/${codeLink}.xhtml`);
             codeReq.send();
         }
     }
