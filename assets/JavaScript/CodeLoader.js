c9225c92bd0fd6a07572b3479cd5c04b6bd2cd8a
guleixibian <guleixibian@outlook.com>
2022-04-14 15:15:10 +0800

    Stop using XML because sometimes there is complication error(I don't know why!)

--- assets/JavaScript/CodeLoader.js
+++ assets/JavaScript/CodeLoader.js
@@ -12,13 +12,13 @@ jQuery(function(){
             codeReq.onloadend = function(){
                 var response = this.response;
                 var parser = new DOMParser();
-                var codeExample = parser.parseFromString(response,"text/xml");
-                var code = codeExample.getElementsByTagName("codeExample")[0].innerHTML;
+                var codeExample = parser.parseFromString(response,"text/html");
+                var code = codeExample.getElementsByTagName("main")[0].innerHTML;
                 $(element).find("code").text(code);
                 Example.innerHTML = code.replace(/Lorem ipsum.../,"Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.");
                 setTimeout(Prism.highlightAll(), 100)
             };
-            codeReq.open("GET",`codeExamples/${codeLink}.xml`);
+            codeReq.open("GET",`iframes/${codeLink}.html`);
             codeReq.send();
         }
     }
